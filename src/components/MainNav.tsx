214207078973e8c61a8799ff80bcd0f440db569d
xyphonist <syphonistmaza@gmail.com>
2024-04-18 23:01:58 +0800

    first commit

---
+++ src/components/MainNav.tsx
@@ -0,0 +1,26 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "./ui/button";
+import UsernameMenu from "./UsernameMenu";
+
+const MainNav = () => {
+  const {loginWithRedirect, isAuthenticated} = useAuth0();
+
+  return(
+    <span className="flex space-x-2 items-center">
+      {isAuthenticated ? (
+      <UsernameMenu/> 
+      ):( 
+           <Button 
+           variant="ghost" 
+           className="font-bold hover:text-green-800 hover:bg-white"
+           onClick={async () => await loginWithRedirect()}
+           >
+       Login
+           </Button>
+      )}
+    </span>
+ 
+  )
+}
+
+export default MainNav;
